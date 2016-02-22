04ea96102e34fc7249eb02a8443b259bc9bdbb92
bhagatali <bhagat.ali@gmail.com>
2016-02-22 16:55:54 -0500

    Syncing Up

---
+++ app/facebook/facebook.js
@@ -0,0 +1,29 @@
+'use strict';
+
+angular.module('myApp.facebook', ['ngRoute','ngFacebook'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/facebook', {
+    templateUrl: 'facebook/facebook.html',
+    controller: 'FacebookCtrl'
+  });
+}])
+
+.config( function( $facebookProvider ) {
+  $facebookProvider.setAppId('968053186604029');
+  $facebookProvider.setPermissions("email", "public_profile", "user_posts", "publish_actions", "use_photos");
+})
+
+.run(function($rootscope){
+    (function(d, s, id){
+         var js, fjs = d.getElementsByTagName(s)[0];
+         if (d.getElementById(id)) {return;}
+         js = d.createElement(s); js.id = id;
+         js.src = "//connect.facebook.net/en_US/sdk.js";
+         fjs.parentNode.insertBefore(js, fjs);
+       }(document, 'script', 'facebook-jssdk'));    
+})
+
+.controller('FacebookCtrl', [function() {
+
+}]);
